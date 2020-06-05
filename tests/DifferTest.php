28836c824c63ca243d834e44eacdf668f6e01764
Anton Demianko <demianko.a@gmail.com>
2020-06-05 23:08:40 +0300

    fix tests

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -5,52 +5,71 @@ namespace Differ\tests\DifferTest;
 use PHPUnit\Framework\TestCase;
 
 use function Differ\Differ\genDiff;
-use function Differ\Render\render;
 
 class DifferTest extends TestCase
 {
-    protected $correctJson;
-    protected $correctPretty;
-    protected $correctPlain;
+    /**
+     * @param $format
+     * @param $firstFile
+     * @param $secondFile
+     * @param $correctDiff
+     *
+     * @dataProvider providerDiffData
+     */
 
-    protected function setUp(): void
+    public function testGenDiff($format, $firstFile, $secondFile, $correctDiff)
     {
-        $this->correctJson = file_get_contents($this->getFilePath('correct/correct_json'));
-        $this->correctPretty = file_get_contents($this->getFilePath('correct/correct_pretty'));
-        $this->correctPlain = file_get_contents($this->getFilePath('correct/correct_plain'));
+        $answer = file_get_contents($this->getFilePath($correctDiff));
+
+        $this->assertEquals($answer, genDiff($this->getFilePath($firstFile), $this->getFilePath($secondFile), $format));
     }
 
-    public function getFilePath($path)
+    public function providerDiffData()
     {
-        $pathToDir = __DIR__;
-
-        return "{$pathToDir}/fixtures/{$path}";
+        return [
+            'prettyJsonDiff' => [
+                'pretty',
+                'before.json',
+                'after.json',
+                'correct/correct_pretty'
+            ],
+            'plainJsonDiff' => [
+                'plain',
+                'before.json',
+                'after.json',
+                'correct/correct_plain'
+            ],
+            'jsonJsonDiff' => [
+                'json',
+                'before.json',
+                'after.json',
+                'correct/correct_json'
+            ],
+            'prettyYamlDiff' => [
+                'pretty',
+                'before.yaml',
+                'after.yaml',
+                'correct/correct_pretty'
+            ],
+            'plainYamlDiff' => [
+                'plain',
+                'before.yaml',
+                'after.yaml',
+                'correct/correct_plain'
+            ],
+            'jsonYamlDiff' => [
+                'json',
+                'before.yaml',
+                'after.yaml',
+                'correct/correct_json'
+            ]
+        ];
     }
 
-    public function testPlainDiff()
+    private function getFilePath($filePath)
     {
-        $jsonDiff = genDiff($this->getFilePath('before.json'), $this->getFilePath('after.json'), 'plain');
-        $yamlDiff = genDiff($this->getFilePath('before.yaml'), $this->getFilePath('after.yaml'), 'plain');
+        $dir = __DIR__;
 
-        $this->assertEquals($this->correctPlain, $jsonDiff);
-        $this->assertEquals($this->correctPlain, $yamlDiff);
-    }
-
-    public function testPrettyDiff()
-    {
-        $jsonDiff = genDiff($this->getFilePath('before.json'), $this->getFilePath('after.json'), 'pretty');
-        $yamlDiff = genDiff($this->getFilePath('before.yaml'), $this->getFilePath('after.yaml'), 'pretty');
-
-        $this->assertEquals($this->correctPretty, $jsonDiff);
-        $this->assertEquals($this->correctPretty, $yamlDiff);
-    }
-
-    public function testJsonDiff()
-    {
-        $jsonDiff = genDiff($this->getFilePath('before.json'), $this->getFilePath('after.json'), 'json');
-        $yamlDiff = genDiff($this->getFilePath('before.yaml'), $this->getFilePath('after.yaml'), 'json');
-
-        $this->assertEquals($this->correctJson, $jsonDiff);
-        $this->assertEquals($this->correctJson, $yamlDiff);
+        return "{$dir}/fixtures/{$filePath}";
     }
 }
